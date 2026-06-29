0315997dcf5042c05c1633a2184667edfc188ea8
monazarea <monazarea3@gmail.com>
2026-06-29 09:48:54 -0400

    fix(data): remove mistakenly added CustomerDTO

--- TrueFit/Packages/Data/DTOs/Remote/File.swift
+++ TrueFit/Packages/Data/DTOs/Remote/File.swift
@@ -5,17 +5,4 @@
 //  Created by Omar Khaled Jaafar on 27/06/2026.
 //
 
-struct CustomerDTO: Decodable {
-    let id: String
-    let email: String
-    let firstName: String
-    let lastName: String
-    let avatarURL: String?
 
-    enum CodingKeys: String, CodingKey {
-        case id, email
-        case firstName = "first_name"
-        case lastName  = "last_name"
-    
-    }
-}
