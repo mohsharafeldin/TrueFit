ef4f8e0712313990413a0a2e0c344243dec3b3ff
OmarKhaled2092001 <omark5787@gmail.com>
2026-07-07 07:08:08 -0700

    feat(ai-chat): add ChatMessage domain model

---
+++ TrueFit/Packages/Domain/Entities/AIChat/ChatMessage.swift
@@ -0,0 +1,21 @@
+//
+//  ChatMessage.swift
+//  TrueFit
+//
+//  Created by Omar Khaled Jaafar on 07/07/2026.
+//
+
+import Foundation
+
+struct ChatMessage: Identifiable, Equatable {
+    let id = UUID()
+    let text: String
+    let isUser: Bool
+    let date: Date
+    
+    init(text: String, isUser: Bool, date: Date = Date()) {
+        self.text = text
+        self.isUser = isUser
+        self.date = date
+    }
+}
